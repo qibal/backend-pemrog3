65f31b36448f4984eac6e52b436e51f22e910ab5
qibal <iqbalherlambang.39@gmail.com>
2025-07-07 14:38:12 +0700

    cord front end

--- config/cors.go
+++ config/cors.go
@@ -2,7 +2,7 @@ package config
 
 var allowedOrigins = []string{
 	"http://localhost:5173/",
-	"backend-pemrog3-production.up.railway.app",
+	"https://front-end-pemprog3.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
